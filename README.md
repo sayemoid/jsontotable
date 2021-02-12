fccbe29f7d1d06d6141d0f9d78a55dfd375dfe2a
Sayem Hossain <sayemkcn@gmail.com>
2021-02-12 10:37:39 +0600

    [Add] readme

--- README.md
+++ README.md
@@ -1,2 +1,90 @@
 # jsontotable
 This library converts json object or array to html table
+
+## Example
+
+### Input
+```aidl
+[
+  {
+    "UserID": 1,
+    "UserName": "rooter",
+    "Password": "12345",
+    "Country": "UK",
+    "Email": "sac@gmail.com"
+  },
+  {
+    "UserID": 2,
+    "UserName": "binu",
+    "Password": "123",
+    "Country": "uk",
+    "Email": "Binu@gmail.com"
+  },
+  {
+    "UserID": 3,
+    "UserName": "cal",
+    "Password": "123",
+    "Country": "uk",
+    "Email": "cal@gmail.com"
+  },
+  {
+    "UserID": 4,
+    "UserName": "nera",
+    "Password": "1234",
+    "Country": "uk",
+    "Email": "nera@gmail.com"
+  }
+]
+```
+
+### Convert
+String html = HtmlTable.fromJson(json);
+
+### Output
+```aidl
+<table class="table table-striped">
+	<tr>
+		<th>UserName</th>
+		<th>Email</th>
+		<th>UserID</th>
+		<th>Country</th>
+		<th>Password</th>
+	</tr>
+	<tbody>
+		<tr>
+			<td>rooter</td>
+			<td>sac@gmail.com</td>
+			<td>1</td>
+			<td>UK</td>
+			<td>12345</td>
+		</tr>
+		<tr>
+			<td>binu</td>
+			<td>Binu@gmail.com</td>
+			<td>2</td>
+			<td>uk</td>
+			<td>123</td>
+		</tr>
+		<tr>
+			<td>cal</td>
+			<td>cal@gmail.com</td>
+			<td>3</td>
+			<td>uk</td>
+			<td>123</td>
+		</tr>
+		<tr>
+			<td>nera</td>
+			<td>nera@gmail.com</td>
+			<td>4</td>
+			<td>uk</td>
+			<td>1234</td>
+		</tr>
+	</tbody>
+</table>
+
+```
+
+![img.png](img.png)
+
+
+
--- src/main/java/dev/sayem/jsontotable/Main.java
+++ src/main/java/dev/sayem/jsontotable/Main.java
@@ -2,13 +2,36 @@ package dev.sayem.jsontotable;
 
 public class Main {
     public static void main(String[] args) {
-        String json = " {\n" +
+        String json = "[\n" +
+                "  {\n" +
                 "    \"UserID\": 1,\n" +
                 "    \"UserName\": \"rooter\",\n" +
                 "    \"Password\": \"12345\",\n" +
                 "    \"Country\": \"UK\",\n" +
                 "    \"Email\": \"sac@gmail.com\"\n" +
-                "  }";
+                "  },\n" +
+                "  {\n" +
+                "    \"UserID\": 2,\n" +
+                "    \"UserName\": \"binu\",\n" +
+                "    \"Password\": \"123\",\n" +
+                "    \"Country\": \"uk\",\n" +
+                "    \"Email\": \"Binu@gmail.com\"\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"UserID\": 3,\n" +
+                "    \"UserName\": \"cal\",\n" +
+                "    \"Password\": \"123\",\n" +
+                "    \"Country\": \"uk\",\n" +
+                "    \"Email\": \"cal@gmail.com\"\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"UserID\": 4,\n" +
+                "    \"UserName\": \"nera\",\n" +
+                "    \"Password\": \"1234\",\n" +
+                "    \"Country\": \"uk\",\n" +
+                "    \"Email\": \"nera@gmail.com\"\n" +
+                "  }\n" +
+                "]";
         System.out.println(HtmlTable.fromJson(json));
     }
 }
